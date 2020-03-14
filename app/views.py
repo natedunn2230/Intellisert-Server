3d685809a02f56f6f6d50ced594b484cc76ac034
natedunn2230 <ntd2230@gmail.com>
2020-03-14 14:01:20 -0400

    Created basic endpoint for the server

--- app/views.py
+++ app/views.py
@@ -3,15 +3,4 @@ from app import light_controller
 
 @app.route('/')
 def home():
-    return "welcome to basic server. Type 192.168.0.21:5000/light<action> (on, off) to control light"
-
-@app.route('/light/<action>')
-def increase_flicker(action):
-   if action == 'on':
-       light_controller.turn_on()
-       return "light activated"
-   elif action == 'off':
-       light_controller.turn_off()
-       return "light de-activated"
-   else:
-       return 'unknown command \"' + action +  '\"'
+    return "Welcome to Intellisert!"
