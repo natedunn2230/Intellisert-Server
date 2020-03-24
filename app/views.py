a22392e7b65bac54ae394568a337cb3a00afc522
natedunn2230 <ntd2230@gmail.com>
2020-03-24 18:16:17 -0400

    created endpoint for handling light on/off

--- app/views.py
+++ app/views.py
@@ -1,6 +1,34 @@
 from app import app
 from app import light_controller
 
+from flask import jsonify
+from flask import request
+
+# HOME ROUTE
 @app.route('/')
 def home():
-    return "Welcome to Intellisert!"
+    return jsonify({"msg": "Welcome to Intellisert!"}), 200
+
+
+# LIGHT ROUTES
+@app.route('/light/power', methods=["POST"])
+def light():
+    
+    if request.is_json:
+
+        data = request.get_json()
+
+        print(f"Recieved {data}")
+        try:
+            power = data["power"]
+            ## Do light controller stuff
+            
+            return jsonify({"msg": "success"}), 200
+        
+        except Exception as E:
+            print(E)
+            return jsonify({"msg": "Bad request. Expecting Key 'power'"}), 400
+    
+
+    return jsonify({"msg": "Bad request. Expecting application/json content"}), 400
+    
