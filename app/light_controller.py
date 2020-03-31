deaff15c68351c56ded0e321e1da55878786d1d2
natedunn2230 <ntd2230@gmail.com>
2020-03-31 15:01:29 -0400

    Added an endpoint to retrieve the light's state

--- app/light_controller.py
+++ app/light_controller.py
@@ -6,10 +6,17 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(21, GPIO.OUT)
 
+class Controller: 
+    def __init__(self):
+        self.power = 'off'
 
-def turn_on():
-    GPIO.output(21, GPIO.HIGH)
+    def turn_on(self):
+        GPIO.output(21, GPIO.HIGH)
+        self.power = 'on'
 
-def turn_off():
-    GPIO.output(21, GPIO.LOW)
+    def turn_off(self):
+        GPIO.output(21, GPIO.LOW)
+        self.power = 'off'
 
+    def get_state(self):
+        return {'power': self.power}
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from app import app
-from app import light_controller
+from app.light_controller import Controller
 
 from flask import jsonify
 from flask import request
+
+# light controller
+controller = Controller()
 
 # HOME ROUTE
 @app.route('/')
@@ -11,8 +14,17 @@ def home():
 
 
 # LIGHT ROUTES
+@app.route('/light-state', methods=["GET"])
+def light_state():
+    '''
+        Returns the state of the light, such as if it is
+        on or not
+    '''
+    state = controller.get_state()
+    return jsonify(state), 200
+
 @app.route('/light/power', methods=["POST"])
-def light():
+def light_power():
     '''
         Accepts boolean 'power' and dispatches the light controller
         to either turn the light on or off
@@ -27,9 +39,9 @@ def light():
             ## Do light controller stuff
 
             if power == "on":
-                light_controller.turn_on()
+                controller.turn_on()
             elif power == "off":
-                light_controller.turn_off()
+                controller.turn_off()
             else:
                 raise Exception('invalid value')
 
@@ -41,4 +53,3 @@ def light():
     
 
     return jsonify({"msg": "Bad request. Expecting application/json content"}), 400
-    
